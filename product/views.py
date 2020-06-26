58b0ac9b18c494175dbf8cb1683efb246d0e8ec1
박정아 <>
2020-06-26 17:33:36 +0900

    경로 다시잡고 재업로드

---
+++ product/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
+from django.views.generic.edit import FormView
+from django.utils.decorators import method_decorator
+from fcuser.decorators import admin_required
+from .models import Product
+from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
+
+# Create your views here.
+
+class ProductList(ListView):
+    model = Product
+    template_name = 'product.html'
+
+@method_decorator(admin_required,name='dispatch')
+class ProdcutCreate(FormView):
+    template_name = 'register_product.html'
+    form_class = RegisterForm
+    success_url = '/product/'
+
+class ProdcutDetail(DeleteView):
+    template_name = 'product_detail.html'
+    queryset = Product.objects.all()
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = OrderForm(self.request)
+        return context
