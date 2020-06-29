5d52a0db2234e1f390c71b00064037e75b6cb05d
박정아 <>
2020-06-29 16:27:18 +0900

    시크릿추가

---
+++ first_project/fc_django/security.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'oe)#!0sdm60v4k+=xpa@19)_-5^mdx=@7=*lylqyod3haj_&vp'
--- first_project/fc_django/settings.py
+++ first_project/fc_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .security import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oe)#!0sdm60v4k+=xpa@19)_-5^mdx=@7=*lylqyod3haj_&vp'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
